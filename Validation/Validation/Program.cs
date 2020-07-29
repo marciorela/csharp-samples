8a9b32641f03f02d2eae8b2746f6467a230772d2
Marcio Rela <marciorela75@gmail.com>
2020-07-29 20:34:19 -0300

    Validacao de propriedades com data annotations

---
+++ Validation/Validation/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Validation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var usuario = new UsuarioForm()
+            {
+                Salario = 4599,
+                Email = "r"
+            };
+
+            var listaMensagens = new List<ValidationResult>();
+            var contexto = new ValidationContext(usuario);
+            bool isValid = Validator.TryValidateObject(usuario, contexto, listaMensagens, true);
+
+            listaMensagens.ForEach(m => Console.WriteLine(m));
+        }
+    }
+}
---
+++ Validation/Validation/UsuarioForm.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Validation
+{
+    public class UsuarioForm
+    {
+        [Required(ErrorMessage = "Nome deve ser informado.")]
+        public string Nome { get; set; }
+
+        [EmailAddress(ErrorMessage = "Formato de e-mail inválido.")]
+        public string Email { get; set; }
+
+        [Range(2000, 8000, ErrorMessage = "Valor de salário fora da faixa")]
+        public double Salario { get; set; }
+    }
+}
