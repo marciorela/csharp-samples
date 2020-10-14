764d6e9fe0ccc8178647e54ae528ab27dc0cb0bf
Marcio Rela <marciorela75@gmail.com>
2020-10-14 09:15:31 -0300

    Carregamento de CSV/Positional com Attributes

---
+++ CarregarCSV/CarregarCSV/CSVExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace CSVExtensions
+{
+    static class CSVExtensions
+    {
+        public static T FromCSV<T>(this string csv) where T : new()
+        {
+            //csv = ConverterStringAcentuada(850, csv);
+
+            var result = new T();
+            var values = Regex.Split(csv, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+
+            Type type = result.GetType();
+            var props = type.GetProperties();
+            for (var i = 0; i < values.Length; i++)
+            {
+                props[i].SetValue(result, values[i].Replace("\"", ""));
+            }
+
+            return result;
+
+        }
+
+        // CodePage - 850
+        public static string ConverterStringAcentuada(int codepage, string texto)
+        {
+            return System.Text.Encoding.ASCII.GetString(System.Text.Encoding.GetEncoding(codepage).GetBytes(texto));
+        }
+
+        public static T FromPositional<T>(this string csv) where T : new()
+        {
+            var result = new T();
+
+            var type = result.GetType();
+            var props = type.GetProperties();
+            foreach (var prop in props)
+            {
+                var attr = prop.GetCustomAttribute<PositionalAttribute>();
+                if (attr != null)
+                {
+                    prop.SetValue(result, csv.Substring(attr.Start-1, attr.Length));
+                }
+            }
+
+            return result;
+        }
+
+    }
+
+    public sealed class PositionalAttribute : Attribute
+    {
+        public readonly int Start;
+        public readonly int Length;
+
+        public PositionalAttribute(int start, int length)
+        {
+            this.Start = start;
+            this.Length = length;
+        }
+    }
+}
---
+++ CarregarCSV/CarregarCSV/Program.cs
@@ -0,0 +1,105 @@
+﻿using CSVExtensions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace CarregarCSV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarregarArquivoCSV(@"C:\Users\mrela\Downloads\Protocolo_Varas\de-para-varas-bacen-cnj.csv");
+            CarregarArquivoPosicional(@"\\jd\Arquivos\Sistemas\JD Sistema Ocorrencias\Evidencias\JD-13\JD-13469\Suporte\Arquivos 5305 Desde 08092020\BJ2VJ1823612020200909231200.TXT");
+        }
+
+        static void CarregarArquivoCSV(string fileName)
+        {
+            using var sr = new StreamReader(fileName);
+
+            while (!sr.EndOfStream)
+            {
+                string s = sr.ReadLine();
+
+                var vara = s.FromCSV<Vara>();
+
+                try
+                {
+                    if (vara.Cod_bacen == "18466")
+                    {
+                        Console.WriteLine(vara.Cod_bacen);
+                        Console.WriteLine(vara.Vara_bacen);
+                        Console.WriteLine(vara.Tribunal_bacen);
+                        Console.WriteLine(vara.Cod_orgao_cnj);
+                        Console.WriteLine(vara.Orgao_cnj);
+                        Console.WriteLine(vara.Percentual_similar);
+                        Console.WriteLine("=====================");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(s);
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
+            }
+
+        }
+
+        static void CarregarArquivoPosicional(string fileName)
+        {
+            using var sr = new StreamReader(fileName);
+
+            while (!sr.EndOfStream)
+            {
+                string s = sr.ReadLine();
+
+                var vara = s.FromPositional<OutraVara>();
+
+                try
+                {
+                    if (vara.TpRegistro == "02")
+                    {
+                        Console.WriteLine(vara.Codigo);
+                        Console.WriteLine("=====================");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(s);
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
+            }
+
+        }
+
+    }
+
+    public class Vara
+    {
+        public string Cod_bacen { get; set; }
+        public string Vara_bacen { get; set; }
+        public string Tribunal_bacen { get; set; }
+        public string Cod_orgao_cnj { get; set; }
+        public string Orgao_cnj { get; set; }
+        public string Percentual_similar { get; set; }
+
+    }
+
+    public class OutraVara
+    {
+        [Positional(1, 2)]
+        public string TpRegistro { get; set; }
+        
+        [Positional(3, 5)]
+        public string Codigo { get; set; }
+
+        [Positional(123, 215)]
+        public string Tribunal { get; set; }
+    }
+
+}
